439b34a986ceec6b7d6f58f23990bf8820edefe8
Freddy Coaricona <fcoaricona@outlook.com>
2024-07-12 21:54:22 -0400

    config: new config cloudflare v3.1.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { montserrat } from "./ui/fonts";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const runtime = "edge";
 
 export const metadata: Metadata = {
   title: "Test NextJS Turso Clf",
